9ea81cdd7368742d7438bae101f56189fa822372
front-end <gerink3@gmail.com>
2018-04-18 04:49:16 +0200

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
 	{
 		path: 'login',
 		component: LoginComponent,
 		// canActivate: [CanActivateViaAuthGuard]
-  },
-  {
+	},
+	{
+		path: 'signup',
+		component: SignupComponent,
+		// canActivate: [CanActivateViaAuthGuard]
+	},	
+	{
 		path: '',
 		redirectTo: '/login', // '/login'
 		pathMatch: 'full'
