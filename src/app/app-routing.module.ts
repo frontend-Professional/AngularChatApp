445012b48152742ac15bac167a6275dd479f9b63
front-end <gerink3@gmail.com>
2018-04-20 14:21:33 +0200

    module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,34 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { MainchatComponent } from './components/mainchat/mainchat.component';
+import { GuestGuard } from './auth/guest.guard';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
 	{
 		path: 'login',
 		component: LoginComponent,
-		// canActivate: [CanActivateViaAuthGuard]
+		canActivate: [GuestGuard]
 	},
 	{
 		path: 'signup',
 		component: SignupComponent,
-		// canActivate: [CanActivateViaAuthGuard]
-	},	
+		canActivate: [GuestGuard]
+	},
+	{
+		path: 'main',
+		component: MainchatComponent,
+		canActivate: [AuthGuard]
+	},
 	{
 		path: '',
-		redirectTo: '/login', // '/login'
+		redirectTo: '/main', // '/login'
+		pathMatch: 'full'
+	},
+	{
+		path: '**',
+		redirectTo: '/main', // '/login'
 		pathMatch: 'full'
 	}
 ]
